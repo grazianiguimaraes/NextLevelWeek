71138e1194ab6fc8edbbdf37c704ed4e65c1cfb5
grazianiguimaraes <grazianiguimaraes@gmail.com>
2020-06-12 22:31:24 -0300

    setup

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -12,9 +12,10 @@ function populaUFs(){
   })
 }
 
+
 populaUFs()
 
-  function getCities(event) {
+function getCities(event) {
     const citySelect = document.querySelector("select[name=city]")
     const stateInput = document.querySelector("[name=state]")
 
@@ -24,13 +25,16 @@ populaUFs()
     stateInput.value = event.target.options[indexOfSelectedState]
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-  
+
+    citySelect.innerHTML = "<option value>Selecione uma Cidade</option>"
+    citySelect.disabled  = true 
+
     fetch(url)
     .then(res => res.json() )
     .then(cities => {
   
       for(const city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
       
       }
       citySelect.disabled = false 
@@ -40,3 +44,50 @@ populaUFs()
   document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities)
+
+  //Itens da Coleta
+  const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+  for(const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+  }
+
+  const collectedItems = document.querySelector("input[name=items]")
+
+  let selectedItem = []
+
+  function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    //Adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+    
+    // Verificar se exstem itens selecionados, se sim
+    // pegar os itens selecionados
+
+    const alreadySelected = selectedItems.findIndex( item => {
+      const itemFound = item == itemId //isso será true ou false
+      return itemFound
+    })
+    
+    // Função simplificada acima
+    //const alreadySelected = selectedItems.FindIndex( item => item == itemId)
+ 
+    // Se já estiver selecionadp
+    if( alreadySelected >= 0) {
+      // Tirar da Seleção
+      const filteredItems = selectedItems.filter( item => {
+        const itemIsDifferent = item != itemId
+        return itemIsDifferent
+      })
+
+      selectedItems = filteredItems
+    } else {
+      selectedItems.push(itemId)
+    }
+  
+   collectedItems.value = selectedItems
+  
+}

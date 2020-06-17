97cfa71e0a73bcb76bb59c3a351c83f8d55831d0
grazianiguimaraes <grazianiguimaraes@gmail.com>
2020-06-17 01:15:25 -0300

    Configurando nunjucks

--- src/server.js
+++ src/server.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const server = express();
+const express = require("express")
+const server = express()
 
 // Configura a pasta Publica
-server.use(express.static("public"));
+server.use(express.static("public"))
 
-const nunjucks = require("nunjucks");
+const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
   express: server,
   noCache: true
-});
+})
 
 
 server.get("/", (req, res) => {
    //res.sendFile(__dirname + "/views/index.html");
-   return res.render("index.html");
-});
+   return res.render("index.html")
+})
 
 server.get("/create-point", (req, res) => {
-  return res.render("create-point.html");
-});
+  return res.render("create-point.html")
+})
 
 server.get("/search", (req, res) => {
-  return res.render("search-results.html");
-});
+  return res.render("search-results.html")
+})
 // Ligar o Servidor
 server.listen(3000);
